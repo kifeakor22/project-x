166eb85940eb0a5f0fcc9ae36b4f502e9e57fec4
Kc <kifeakor@yahoo.com>
2024-11-18 11:26:16 +0000

    update to product lists

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { WhatsAppWidget } from 'react-whatsapp-widget';
+import Button from '@mui/material/Button';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
 
 const Footer = () => {
     const [currentDate, setCurrentDate] = useState(moment().format('MMMM Do, YYYY'));
@@ -36,6 +42,30 @@ const Footer = () => {
         />
             </div>  
           </div>
+          <Button className='navLink'
+              variant="button"
+              href="https://twitter.com/Whiskytopia"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              <TwitterIcon />
+            </Button>
+            <Button className='navLink'
+              variant="button"
+              href=""
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              <InstagramIcon/>
+            </Button>
+            <Button
+            className='navLink'
+              variant="button"
+              href="https://www.youtube.com/@Whiskytopia"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              <YouTubeIcon/>
+            </Button>
+            <Button className=" navLink fa-brands fa-tiktok" href="https://www.tiktok.com/@whiskytopia?is_from_webapp=1&sender_device=pc" target="_blank">
+            </Button>
           <div className="row mt-3">
             <div className="col-12 text-center">
               <small>
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,68 +1,127 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './style.css'
+import './style.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import YouTubeIcon from '@mui/icons-material/YouTube';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 const Nav = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm')); // Check if screen is small
 
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
 
-    return(
-        <>
-        <AppBar
+  return (
+    <>
+      <AppBar
         position="static"
         elevation={0}
         sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-      className='navigation'>
+        className="navigation"
+      >
         <Toolbar sx={{ flexWrap: 'wrap' }}>
-            
-                <img
-                src={require('../products/assets/logo.jpg')} 
-                alt='company logo'
-                />
-        
+          <img
+            src={require('../products/assets/logo.jpg')}
+            alt="company logo"
+            style={{ marginRight: '10px' }}
+          />
 
-          <Typography className='navLink' variant="h6" noWrap sx={{ flexGrow: 1 }}>
+          <Typography
+            className="navLink"
+            variant="h6"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
             WhiskyTopia
           </Typography>
-          <nav>
-            
-            <Button className='navLink'
-              variant="button"
-              href="https://twitter.com/Whiskytopia"
-              sx={{ my: 1, mx: 1.5 }}
+
+          {/* Show Hamburger Menu Icon on Small Screens */}
+          {isSmallScreen ? (
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
             >
-              <TwitterIcon />
-            </Button>
-            <Button className='navLink'
-              variant="button"
-              href=""
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              <InstagramIcon/>
-            </Button>
-            <Button
-            className='navLink'
-              variant="button"
-              href="https://www.youtube.com/@Whiskytopia"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              <YouTubeIcon/>
-            </Button>
-          </nav>
-          <Button className=" navLink fa-brands fa-tiktok" href="https://www.tiktok.com/@whiskytopia?is_from_webapp=1&sender_device=pc" target="_blank">
-            
-          </Button>
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            // Show Navigation Links on Larger Screens
+            <nav>
+              <Button
+                className="navLink"
+                variant="button"
+                href=""
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Novice
+              </Button>
+              <Button
+                className="navLink"
+                variant="button"
+                href=""
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Intermediary
+              </Button>
+              <Button
+                className="navLink"
+                variant="button"
+                href=""
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Expert
+              </Button>
+              <Button
+                className="navLink"
+                variant="button"
+                href=""
+                sx={{ my: 1, mx: 1.5 }}
+              >
+                Region
+              </Button>
+            </nav>
+          )}
         </Toolbar>
       </AppBar>
-        </>
-    )
-}
 
-export default Nav
+      {/* Drawer for Hamburger Menu */}
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <List
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+          sx={{ width: 250 }}
+        >
+          {['Novice', 'Intermediary', 'Expert', 'Region'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton component={Link} to={`/${text.toLowerCase()}`}>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  );
+};
+
+export default Nav;
