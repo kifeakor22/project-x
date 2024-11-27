18f412e6f2efd2da7e9449db375f21a904d010ad
Kc <kifeakor@yahoo.com>
2024-11-27 14:38:02 +0000

    updated product list

--- src/components/deal/index.js
+++ src/components/deal/index.js
@@ -54,7 +54,7 @@ const Deal = () => {
             gap: '30px',
           }}
         >
-          {products.slice(0, 4).map((product) => (
+          {products.slice(0, 3).map((product) => (
             <Box
               key={product.id}
               onClick={() => handleModalOpen(product)}
